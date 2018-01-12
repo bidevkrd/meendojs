e4b6e1071a5f848a66ecda3ef71bd798f0ebdedb
bidevkrd <35360278+bidevkrd@users.noreply.github.com>
2018-01-12 09:41:57 +0300

    init

---
+++ main.js
@@ -0,0 +1,26 @@
+function initapp() {
+    $(document).ready,
+        function() {
+			console.log('complete');
+        }
+    );
+}
+
+function jq() {
+    var s = document.createElement('script');
+    s.setAttribute('src', '//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js');
+    document.addEventListener('DOMContentLoaded', initapp, true);
+    document.head.appendChild(s);
+}
+
+function injectform() {
+    $('body').append(
+        $();
+    );
+}
+
+function hideforeign() {
+
+}
+
+jq();

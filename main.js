76dcff3ff3e362c9cba03a6fd0ef1397fc8313d9
bidevkrd <35360278+bidevkrd@users.noreply.github.com>
2018-01-12 09:52:43 +0300

    1.7.2 -> 1.9.1

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function initapp() {
 
 function jq() {
     var s = document.createElement('script');
-    s.setAttribute('src', '//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js');
+    s.setAttribute('src', '//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js');
     document.addEventListener('DOMContentLoaded', initapp, true);
     document.head.appendChild(s);
 }
